fcda4dd65f4a1e52d305eea8b0805fb8befdf901
sriram <lnsriram777@gmail.com>
2023-10-22 00:54:48 +0530

    Changes to autocompletion formatting

--- lua/user/core/plugins/nvim-cmp.lua
+++ lua/user/core/plugins/nvim-cmp.lua
@@ -1,3 +1,33 @@
+-- Icons to display the kind of the completion
+local kind_icons = {
+  Text = "󰉿",
+	Method = "󰆧",
+	Function = "󰊕",
+	Constructor = "",
+  Field = " ",
+	Variable = "󰀫",
+	Class = "󰠱",
+	Interface = "",
+	Module = "",
+	Property = "󰜢",
+	Unit = "󰑭",
+	Value = "󰎠",
+	Enum = "",
+	Keyword = "󰌋",
+  Snippet = "",
+	Color = "󰏘",
+	File = "󰈙",
+  Reference = "",
+	Folder = "󰉋",
+	EnumMember = "",
+	Constant = "󰏿",
+  Struct = "",
+	Event = "",
+	Operator = "󰆕",
+  TypeParameter = " ",
+	Misc = " ",
+}
+
 return {
   { 
     'hrsh7th/nvim-cmp',
@@ -22,6 +52,25 @@ return {
 				{ name = "buffer"	},
 				{ name = "path"		},
 			}),
+			formatting = {
+				fields = { "kind", "abbr", "menu" },
+				format = function(entry, vim_item)
+				  -- Kind icons
+				  vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+				  vim_item.menu = ({
+					nvim_lsp = "[LSP]",
+					luasnip = "[Snippet]",
+					buffer = "[Buffer]",
+					path = "[Path]",
+				  })[entry.source.name]
+				  return vim_item
+				end,
+		  },
+			window = {
+				documentation = {
+				border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+				},
+		  },
 		})
 		cmp.setup.cmdline(":", {
 			mapping = cmp.mapping.preset.cmdline(),
