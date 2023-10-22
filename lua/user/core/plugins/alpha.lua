a6f1ca9d9165190e5c519b574402427a1b0d6db8
sriram <lnsriram777@gmail.com>
2023-10-22 15:49:24 +0530

    Adding session manager

--- lua/user/core/plugins/alpha.lua
+++ lua/user/core/plugins/alpha.lua
@@ -27,6 +27,7 @@ return {
 			dashboard.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
 			dashboard.button( "f", "  Find file", ":Telescope find_files<CR>"),
 			dashboard.button( "r", "  Recent"   , ":Telescope oldfiles<CR>"),
+			dashboard.button( "s", "󱑆  Restore last session"   , ":SessionRestore<CR>"),
 			dashboard.button( "q", "  Quit NVIM", ":qa<CR>"),
 		}
 		require("alpha").setup(dashboard.config)
