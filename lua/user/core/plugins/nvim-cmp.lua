66379e14eb135686d5ce1c5183b1a0a70ca5cb9c
sriram <lnsriram777@gmail.com>
2023-11-14 12:32:15 +0530

    Changes to treesitter and nvim cmp

--- lua/user/core/plugins/nvim-cmp.lua
+++ lua/user/core/plugins/nvim-cmp.lua
@@ -29,58 +29,84 @@ local kind_icons = {
 }
 
 return {
-  { 
-    'hrsh7th/nvim-cmp',
-    dependencies = { 'hrsh7th/cmp-nvim-lsp','hrsh7th/cmp-buffer', 'hrsh7th/cmp-path','hrsh7th/cmp-cmdline', 'L3MON4D3/LuaSnip' },
-	config = function()
-		local cmp = require("cmp")
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body)			-- Using luasnip as snippet engine
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-k>"]	 = cmp.mapping.select_prev_item(),
-				["<C-j>"]	 = cmp.mapping.select_next_item(),
-				["<tab>"]	 = cmp.mapping.select_next_item(),
-				["<C-e>"]	 = cmp.mapping.abort(),
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip"  },
-				{ name = "buffer"	},
-				{ name = "path"		},
-			}),
-			formatting = {
-				fields = { "kind", "abbr", "menu" },
-				format = function(entry, vim_item)
-				  -- Kind icons
-				  vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-				  vim_item.menu = ({
-					nvim_lsp = "[LSP]",
-					luasnip = "[Snippet]",
-					buffer = "[Buffer]",
-					path = "[Path]",
-				  })[entry.source.name]
-				  return vim_item
-				end,
-		  },
-			window = {
-				documentation = {
-				border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+	{ 
+		'hrsh7th/nvim-cmp',
+		dependencies = { 'hrsh7th/cmp-nvim-lsp','hrsh7th/cmp-buffer', 'hrsh7th/cmp-path','hrsh7th/cmp-cmdline', 'L3MON4D3/LuaSnip' },
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			require ('luasnip.loaders.from_vscode').load({ paths = "./snippet_folder/"})
+			luasnip.config.setup{}
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)			-- Using luasnip as snippet engine
+					end,
 				},
-		  },
-		})
-		cmp.setup.cmdline(":", {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = cmp.config.sources({
-				{ name = "path" },
+				mapping = cmp.mapping.preset.insert {
+					['<C-n>'] = cmp.mapping.select_next_item(),
+					['<C-p>'] = cmp.mapping.select_prev_item(),
+					['<C-d>'] = cmp.mapping.scroll_docs(-4),
+					['<C-f>'] = cmp.mapping.scroll_docs(4),
+					['<C-Space>'] = cmp.mapping.complete {},
+					['<CR>'] = cmp.mapping.confirm {
+						behavior = cmp.ConfirmBehavior.Replace,
+						select = true,
+					},
+					['<Tab>'] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_locally_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { 'i', 's' }),
+					['<S-Tab>'] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { 'i', 's' }),
+				},
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip"  },
+					{ name = "buffer"	},
+					{ name = "path"		},
+				}),
+				formatting = {
+					fields = { "kind", "abbr", "menu" },
+					format = function(entry, vim_item)
+						-- Kind icons
+						vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+						vim_item.menu = ({
+							nvim_lsp = "[LSP]",
+							luasnip = "[Snippet]",
+							buffer = "[Buffer]",
+							path = "[Path]",
+						})[entry.source.name]
+						return vim_item
+					end,
+				},
+				window = {
+					documentation = {
+						border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+					},
+				},
+			})
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
 				},
 				{
-				{ name = "cmdline" },
-			}),
-		})
-	end,
-  },
+					{ name = "cmdline" },
+				}),
+			})
+		end,
+	},
 }
