8db987567cf74237927834d0f31a8a7ac2c063f6
sriram <lnsriram777@gmail.com>
2023-12-10 18:56:43 +0530

    Changed to NordFox colorscheme'

--- lua/user/core/plugins/colorscheme.lua
+++ lua/user/core/plugins/colorscheme.lua
@@ -1,21 +1,18 @@
 -- COLORSCHEME configs --
 return {
 	{
-		"folke/tokyonight.nvim",
+		"EdenEast/nightfox.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			require("tokyonight").setup({
-				transparent = true,
-				style = "moon",
-				styles = {
-					comments = { italic = false },
-					keywords = { italic = false },
+			require("nightfox").setup({
+				options = {
+					transparent = false,
 				}
 			})
-			vim.cmd[[colorscheme tokyonight]]
-			vim.cmd[[highlight ColorColumn guibg=#34495E]]
+			vim.cmd[[colorscheme nordfox]]
+			vim.cmd[[highlight ColorColumn guibg=#808080]]
 		end,
-	},
+	}
 }
 
